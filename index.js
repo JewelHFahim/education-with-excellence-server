230b000e29b34b991f540832cdb55ecf504a558c
JewelHFahim <jewelhfahim@gmail.com>
2022-10-26 02:13:37 +0600

    category added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 app.use(cors());
-const cources = require('./data.json')
+const cources = require('./data.json');
+const categories = require('./categories.json');
 
 app.get('/', (req, res)=>{
     res.send('Cource API working GREAT!!')
@@ -11,6 +12,18 @@ app.get('/', (req, res)=>{
 
 app.get('/cources', (req, res)=>{
     res.send(cources)
+})
+
+app.get('/cource/:id', (req, res)=>{
+    const id = req.params.id;
+    const singleCource = cources.find(c => c.id == id );
+    res.send(singleCource);
+})
+
+app.get('/category/:id', (req, res)=>{
+    const id = req.params.id;
+    const getCategory = categories.filter(d => d.id == id );
+    res.send(getCategory);
 })
 
 app.listen(port, ()=>{
